588e4a6b3c5d7b2df6c78f64ae9e236ce2451273
wenhao.wu@Mack <wenhaowu1989@hotmail.com>
2015-04-26 21:26:08 -0700

    Factor PEP update test succeeded.
    Now nothing is standing between me and...
    
    the rest part of this simulation...

--- Test_get_factor_PEP_update.m
+++ Test_get_factor_PEP_update.m
@@ -26,7 +26,7 @@ d1 = 0.5; % Distance between S and R
 d2 = 0.5; % Distance between R and D
 nu = 3; % Pathloss factor
 
-N = 5000; % Number of monte-carlo run
+N = 50000; % Number of monte-carlo run
 
 %% 2. Initialization
 Q = 2 ^ Nbps;
@@ -41,6 +41,24 @@ beta_rd = d2 ^ -nu;
 
 g = sqrt(Pr / (beta_sr + beta_rd + sigma_sqr_r)); % The power normalization factor
 
+gamma_sr = abs(sqrt(beta_sr / 2) * (randn(N, 1) + 1i * randn(N, 1))) .^ 2; % Generate the channel absolute value square
+gamma_rd = abs(sqrt(beta_rd / 2) * (randn(N, 1) + 1i * randn(N, 1))) .^ 2;
 %% 3. Test some  E
 dist_sqr = (0 : 0.1 : 1);
-E = get_factor_PEP_update(dist_sqr, beta_sr, beta_rd, g, sigma_sqr_d, sigma_sqr_r)
+n_dist_sqr = length(dist_sqr);
+
+E_analytical = get_factor_PEP_update(dist_sqr, beta_sr, beta_rd, g, sigma_sqr_d, sigma_sqr_r);
+
+E_montecarlo = zeros(1, n_dist_sqr);
+for i_dist_sqr = 1 : n_dist_sqr
+    E_montecarlo(i_dist_sqr) = mean(exp(-g ^ 2 / 4 * dist_sqr(i_dist_sqr) * gamma_sr .* gamma_rd ./ (sigma_sqr_d + g ^ 2 * sigma_sqr_r * gamma_rd)));
+end
+
+%% Visualization
+figure;
+plot(dist_sqr, E_analytical, 'b-', 'linewidth', 2), hold on;
+plot(dist_sqr, E_montecarlo, 'ro--', 'linewidth', 2);
+grid on;
+set(gca, 'fontsize', 16);
+xlabel('\epsilon'), ylabel('E');
+legend('Analytical', 'Monte-carlo');
